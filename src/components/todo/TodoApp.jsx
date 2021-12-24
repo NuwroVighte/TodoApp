b64dec29e29de87e5c08a82868af34adbc47b38d
nuwro <droberts3527@gmail.com>
2021-12-24 09:22:48 -0600

    added controlled component to handle username changes with default username

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -10,12 +10,28 @@ class TodoApp extends Component {
     }
 }
 
+
 class LoginComponent extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: 'droberts',
+            password: ''
+        }
+        this.handleUsernameChange = this.handleUsernameChange.bind(this)
+    }
+
+    handleUsernameChange(event) {
+        console.log(event.target.value)
+        this.setState({ username: event.target.value })
+
+    }
+
     render () {
         return (
             <div>
-            Username: <input type="text" name="username" />
-            Password: <input type="password" name="password" />
+            Username: <input type="text" name="username" value={this.state.username} onChange={this.handleUsernameChange}/>
+            Password: <input type="password" name="password" value={this.state.password}/>
             <button>Login</button>
             </div>
         )
