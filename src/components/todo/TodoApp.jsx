b1b15da3994ef4d715cc3aec476b4254fec929d0
nuwro <droberts3527@gmail.com>
2022-01-28 16:38:02 -0600

    HeaderComponent set up, working. Needs usernamevar in welcome link, logout page connect

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import {useState} from 'react'
-import {BrowserRouter as Router, Routes, Route, useNavigate} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route, useNavigate, Link} from 'react-router-dom'
 import WelcomeComponent from './WelcomeComponent'
 import LoginComponent from './LoginComponent'
 
@@ -33,14 +33,18 @@ class HeaderComponent extends Component {
     render() {
         return (
             <header>
-                <nav>
-
+                <nav className = "navbar navbar-expand-md navbar-dark bg-dark">
+                    <div><a href="https://github.com/NuwroVighte" className="navbar-brand">My Github</a></div>
+                    <ul className="navbar-nav">
+                        <li><Link className="nav-link" to="/welcome/droberts">Home</Link></li> {/*TODO: replace droberts with username var*/}
+                        <li><Link className="nav-link" to="/todos">Todos</Link></li>
+                    </ul>
+                    <ul className="navbar-nav navbar-collapse justify-content-end">
+                        <li><Link className="nav-link" to="/login">Login</Link></li>
+                        <li><Link className="nav-link" to="/logout">Logout</Link></li>
+                    </ul>
                 </nav>
             </header>
-
-            // <div>
-            //     Header <hr/>
-            // </div>
         )
     }
 }
