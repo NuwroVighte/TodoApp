eb5acf476ba275bb04c40693da3cf9b267e0f9e0
nuwro <droberts3527@gmail.com>
2022-01-31 23:54:45 -0600

    connected ListTodosComponent w/ spring boot api

--- src/components/todo/ListTodosComponent.jsx
+++ src/components/todo/ListTodosComponent.jsx
@@ -1,21 +1,40 @@
+import moment from 'moment'
 import React, {Component} from 'react'
+import TodoDataService from '../../api/todo/TodoDataService'
+import AuthenticationService from './AuthenticationService'
 
 
 class ListTodosComponent extends Component {
     constructor(props) {
+        console.log('constructor')
         super(props)
         this.state = {
             todos : 
             [
-             {id: 1, description : 'Update to function components', done: false, targetDate: new Date()},
-             {id: 2, description : 'Refactor SQL queries', done: false, targetDate: new Date()},
-             {id: 3, description : 'Practice leetcode', done: false, targetDate: new Date()},
-             {id: 4, description : 'Pet dog', done: false, targetDate: new Date()}
+            //  {id: 1, description : 'Update to function components', done: false, targetDate: new Date()},
+            //  {id: 2, description : 'Refactor SQL queries', done: false, targetDate: new Date()},
+            //  {id: 3, description : 'Practice leetcode', done: false, targetDate: new Date()},
+            //  {id: 4, description : 'Pet dog', done: false, targetDate: new Date()}
             ]
         }
     }
 
+    componentDidMount() {
+        console.log('componentDidMount')
+        let username = AuthenticationService.getLoggedInUserName
+        TodoDataService.retrieveAllTodos(username)
+        .then(
+            response => {
+                console.log(response)
+                this.setState({
+                    todos: response.data
+                })
+            }
+        )
+    }
+
     render() {
+        console.log('render')
         return(
             <div>
                 <h1>List of todos.</h1>
@@ -25,7 +44,7 @@ class ListTodosComponent extends Component {
                             <tr>
                                 <th>Description</th>
                                 <th>Done?</th>
-                                <th>Date</th>
+                                <th>Target Date</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -35,7 +54,7 @@ class ListTodosComponent extends Component {
                                     <tr key={todo.id}>
                                     <td>{todo.description}</td>
                                     <td>{todo.done.toString()}</td>
-                                    <td>{todo.targetDate.toString()}</td>
+                                    <td>{moment(todo.targetDate).format('MMM DD, YYYY')}</td>
                                     </tr>
                                 )
                             }
--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,11 +1,18 @@
 import React, {Component} from 'react'
 import {useParams, Link} from 'react-router-dom'
+import HelloWorldService from '../../api/todo/HelloWorldService'
 
 class WelcomeComponent extends Component {
     constructor(props) {
         super(props)
 
         this.retrieveWelcomeMessage = this.retrieveWelcomeMessage.bind(this)
+        this.handleSuccessfulResponse = this.handleSuccessfulResponse.bind(this)
+        this.handleError = this.handleError.bind(this)
+
+        this.state = {
+            welcomeMessage: ''
+        }
     }
 
 
@@ -21,12 +28,27 @@ class WelcomeComponent extends Component {
                 Click here to be double welcomed!
                 <button onClick={this.retrieveWelcomeMessage} className="btn btn-success">Get EXTRA welcomed!</button>
             </div>
+            <div className="container">
+                {this.state.welcomeMessage}
+            </div>
             </>
         )
     }
 
     retrieveWelcomeMessage() {
-        console.log('regrieve clicked')
+        HelloWorldService.executeHelloWorldPathVariableService(this.props.params.name)
+        .then(response => this.handleSuccessfulResponse(response))
+        .catch(error => this.handleError(error))
+    }
+
+    handleSuccessfulResponse(response) {
+        console.log(response)
+        this.setState({welcomeMessage: response.data.message})
+    }
+
+    handleError(error) {
+        console.log(error.response)
+        this.setState({welcomeMessage: error.response.data.message})
     }
 }
 
