d3a04722c894de836617f7dabe07b2378b799f3c
nuwro <droberts3527@gmail.com>
2022-02-14 04:01:59 -0600

    pre H2 commit

--- src/components/todo/AuthenticationService.js
+++ src/components/todo/AuthenticationService.js
@@ -1,4 +1,7 @@
 import axios from 'axios'
+import {API_URL} from '../../Constants'
+
+export const USER_NAME_SESSION_ATTRIBUTE_NAME = 'authenticatedUser'
 
 class AuthenticationService {
 
@@ -7,7 +10,7 @@ class AuthenticationService {
     }
 
     executeBasicAuthenticationService(username, password) {
-        return axios.get('http://localhost:8080/basicauth', {headers: {authorization: this.createBasicAuthToken(username, password)}})
+        return axios.get(`${API_URL}/basicauth`, {headers: {authorization: this.createBasicAuthToken(username, password)}})
     }
 
     createJWTToken(token) {
@@ -15,34 +18,34 @@ class AuthenticationService {
     }
 
     executeJwtAuthenticationService(username, password) {
-        return axios.post('http://localhost:8080/authenticate', { 
+        return axios.post(`${API_URL}/authenticate`, { 
             username,
             password
         })
     }
 
     registerSuccessfulLogin(username, password) {
-        sessionStorage.setItem('authenticatedUser', username)
+        sessionStorage.setItem(USER_NAME_SESSION_ATTRIBUTE_NAME, username)
         this.setupAxiosInterceptors(this.createBasicAuthToken(username, password))
     }
 
     registerSuccessfulLoginForJwt(username, token) {
-        sessionStorage.setItem('authenticatedUser', username)
+        sessionStorage.setItem(USER_NAME_SESSION_ATTRIBUTE_NAME, username)
         this.setupAxiosInterceptors(this.createJWTToken(token))
     }
 
     logout() {
-        sessionStorage.removeItem('authenticatedUser')
+        sessionStorage.removeItem(USER_NAME_SESSION_ATTRIBUTE_NAME)
     }
 
     isUserLoggedIn() {
-        let user = sessionStorage.getItem('authenticatedUser')
+        let user = sessionStorage.getItem(USER_NAME_SESSION_ATTRIBUTE_NAME)
         if(user===null) return false
         return true
     }
 
     getLoggedInUserName() {
-        let user = sessionStorage.getItem('authenticatedUser')
+        let user = sessionStorage.getItem(USER_NAME_SESSION_ATTRIBUTE_NAME)
         if (user===null) return ''
         return user
     }
